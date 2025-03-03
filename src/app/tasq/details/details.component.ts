96b28d631a1c81db830e2cb626fcf75d440fe83d
DESKTOP-K4PNONG\Praveen T <jeevan.kumar.dev1@gmail.com>
2025-03-03 05:39:29 +0530

    view details added

--- src/app/tasq/details/details.component.ts
+++ src/app/tasq/details/details.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from '../../providers/api.service';
+import moment from 'moment';
+import { environment } from '../../../environments/environment';
+import { SavedocumentsComponent } from '../savedocuments/savedocuments.component';
+import { CommentsComponent } from '../comments/comments.component';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  spinner: boolean = false;
+  viewTicketInfo: any;
+  xpertProfileImg = environment.xpertProfileImg;
 
-  constructor(private modalService : NgbModal){}
- 
+  constructor(private modalService: NgbModal, public activeModal: NgbActiveModal, private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.spinner = true;
+    this.api.getTicketData().subscribe((res: any) => {
+      console.log(res);
+      if (res) {
+        this.viewTicketInfo = res;
+        this.spinner = false;
+      }
+      else {
+        this.spinner = false;
+      }
+    })
+  }
+
+
+  LocalTimeConvertioninHours(DateTime: any) {
+    var utcDate = moment.utc(DateTime);
+    var dateWithTimezone = utcDate.local().format('MM.DD.YY hh:mm A');
+    return dateWithTimezone;
+
+  }
+
+  openDocumentsOrCommentsComponent(data: any,module : any) {
+    this.api.setTicketData(data)
+    if(module == 'documents'){
+       this.modalService.open(SavedocumentsComponent, {
+      windowClass: 'documentModal',
+      size: 'lg',
+      backdrop: 'static',
+      centered: true,
+    });
+    }
+    else if(module == 'comments'){
+       this.modalService.open(CommentsComponent, {
+              windowClass: 'commentsModal',
+              size: 'lg', 
+              backdrop: 'static',
+              centered : true, 
+            });
+    }
+   
+  }
+
+
+  closeModal() {
+    this.activeModal.close();
+  }
 }
