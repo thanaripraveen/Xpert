6bc4ded01b59f3306e19eb19f0b957836d0b0a2b
DESKTOP-K4PNONG\Praveen T <jeevan.kumar.dev1@gmail.com>
2025-03-30 23:30:04 +0530

    status images with labels sample added in users module

--- src/app/tasq/users/users.component.ts
+++ src/app/tasq/users/users.component.ts
@@ -6,9 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-  showFirstDiv = true;
-
-  toggleDivs() {
-    this.showFirstDiv = !this.showFirstDiv;
+  statuses = [
+    "Unassigned", "Open", "In Development", "Resolved", "Closed",
+    "Re open", "Need Clarification", "Waiting For Design", "Testing",
+    "Ready For Release", "Released", "Longterm Fix"
+  ];
+  activeStatus = "Need Clarification";
+  
+  setActiveStatus(status: string) {
+    this.activeStatus = status;
   }
+  
+  getStatusIcon(status: string) {
+    switch (status) {
+      case "Unassigned": return "fas fa-user-slash";
+      case "Open": return "fas fa-folder-open";
+      case "In Development": return "fas fa-code";
+      case "Resolved": return "fas fa-check-circle";
+      case "Closed": return "fas fa-lock";
+      case "Re open": return "fas fa-undo-alt";
+      case "Need Clarification": return "fas fa-question-circle";
+      case "Waiting For Design": return "fas fa-paint-brush";
+      case "Testing": return "fas fa-vial";
+      case "Ready For Release": return "fas fa-rocket";
+      case "Released": return "fas fa-flag-checkered";
+      case "Longterm Fix": return "fas fa-wrench";
+      default: return "";
+    }
+  }
+  
+  
 }
--- src/app/tasq/users/users.module.ts
+++ src/app/tasq/users/users.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +11,7 @@ import { UsersComponent } from './users.component';
     UsersComponent
   ],
   imports: [
-    CommonModule,
+    CommonModule,FormsModule,ReactiveFormsModule,
     UsersRoutingModule
   ]
 })
