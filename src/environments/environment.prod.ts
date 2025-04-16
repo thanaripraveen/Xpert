0883dbababdafececffc2f8ab812fda30616b64f
thanaripraveen <praveent@spyhre.com>
2025-04-16 13:20:12 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,15 +6,15 @@ export const environment = {
   dealerimgUrl: "https://xpert2api.axelautomotive.com/images/Dealer_logo/",
   tasqimgUrl: "https://xpert2api.axelautomotive.com/images/",
 
-  // xpertNodeApi: "https://xpertsupportapi.axelautomotive.com/api/",
-  // xpertNodefiles: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/tasks/",
-  // xpertProfileImg: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/profiles/thumbs/",
-  // axelHelpImagePath: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/axelHelp/",  
+  xpertNodeApi: "https://xpertsupportapi.axelautomotive.com/api/",
+  xpertNodefiles: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/tasks/",
+  xpertProfileImg: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/profiles/thumbs/",
+  axelHelpImagePath: "https://xpertsupportapi.axelautomotive.com/api/v1/resources/axelHelp/",  
   
-  xpertNodeApi: "https://devxpertapi.axelautomotive.com/api/",
-  xpertNodefiles: "https://devxpertapi.axelautomotive.com/api/v1/resources/tasks/",
-  xpertProfileImg: "https://devxpertapi.axelautomotive.com/api/v1/resources/profiles/thumbs/",
-  axelHelpImagePath: "https://devxpertapi.axelautomotive.com/api/v1/resources/axelHelp/",  
+  // xpertNodeApi: "https://devxpertapi.axelautomotive.com/api/",
+  // xpertNodefiles: "https://devxpertapi.axelautomotive.com/api/v1/resources/tasks/",
+  // xpertProfileImg: "https://devxpertapi.axelautomotive.com/api/v1/resources/profiles/thumbs/",
+  // axelHelpImagePath: "https://devxpertapi.axelautomotive.com/api/v1/resources/axelHelp/",  
 
   };
   
